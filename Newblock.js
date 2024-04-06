4d257ccd1b5a11b88cf82ac570a9966e8d6ca456
igjnaj <155215699+igjnaj@users.noreply.github.com>
2024-04-06 22:01:49 +0700

    Update Newblock.js

--- Newblock.js
+++ Newblock.js
@@ -1,10 +1,1 @@
-const allowedDomains = ['facebook.com', 'instagram.com', 'discord.com', 'twitter.com', 'youtube.com', 'animevietsub.us'];
-
-window.addEventListener('DOMContentLoaded', (event) => {
-    const currentTabUrl = window.location.hostname;
-    if (allowedDomains.includes(currentTabUrl)) {
-        console.log('Passed');
-    } else {
-        window.close();
-    }
-});
+window.close();
