4e79c70e6b01fe0a2e1e09f0e96990f5dfd891a6
igjnaj <155215699+igjnaj@users.noreply.github.com>
2024-04-06 22:28:03 +0700

    Delete b.js

--- b.js
+++
@@ -1,7 +0,0 @@
-const searchKeywordElement = document.getElementById('searchkeyword');
-    
-// Check if the element exist
-if (searchKeywordElement) {
-// Change the text content of the element to "Tìm kiếm"
-    searchKeywordElement.textContent = "Tìm kiếm";
-}
