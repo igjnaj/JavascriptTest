0ba49f2321b9be54b94a9820ea1ea97a9d352a23
igjnaj <155215699+igjnaj@users.noreply.github.com>
2024-04-06 21:50:54 +0700

    Update a.js
    another update oh boy

--- a.js
+++ a.js
@@ -1,8 +1,7 @@
-// Select the anchor element you want to change
 var element = document.querySelector('footer p.Copy a');
 
 // Check if the element exists before making changes
 if (element) {
     // Change the text content of the element
-    element.textContent = "BetterAnimeVietSub By klemplus 🔥 - ©️Copyright 2024 Animevietsub. All rights reserved.";
+    element.textContent = "BetterAnimeVietSub By klemplus 🔥\n©️Copyright 2024 Animevietsub. All rights reserved.";
 }
