73b92afd77cbebc3cd5ee0e072f8878089333c50
igjnaj <155215699+igjnaj@users.noreply.github.com>
2024-04-06 20:50:30 +0700

    Update and rename Credit.js to a.js

--- Credit.js
+++
@@ -1,5 +0,0 @@
-// Chatgepete 3.5 shu
-var element = document.querySelector('animevietsub.us###Tp-Wp > div.Body.Container:last-child > footer.Footer:nth-child(2) > div.Container > p.Copy:last-child > a');
-if (element) {
-    element.textContent = "Modded By klemplus - ©️Copyright 2024 Animevietsub. All rights reserved.";
-}
---
+++ a.js
@@ -0,0 +1,8 @@
+// Select the anchor element you want to change
+var element = document.querySelector('footer p.Copy a');
+
+// Check if the element exists before making changes
+if (element) {
+    // Change the text content of the element
+    element.textContent = "Modded By klemplus - ©️Copyright 2024 Animevietsub. All rights reserved.";
+}
