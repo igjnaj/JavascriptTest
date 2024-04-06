8cd1dea8cae3d4c443e6f6a2a8c0bbd1f08ac7c9
igjnaj <155215699+igjnaj@users.noreply.github.com>
2024-04-06 21:55:06 +0700

    Update a.js

--- a.js
+++ a.js
@@ -2,6 +2,6 @@ var element = document.querySelector('footer p.Copy a');
 
 // Check if the element exists before making changes
 if (element) {
-    // Change the text content of the element
-    element.textContent = "BetterAnimeVietSub By klemplus 🔥\n©️Copyright 2024 Animevietsub. All rights reserved.";
+    // Change the HTML content of the element
+    element.innerHTML = "BetterAnimeVietSub By klemplus 🔥<br>©️Copyright 2024 Animevietsub. All rights reserved.";
 }
