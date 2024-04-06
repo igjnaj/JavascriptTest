e596961efe1caaeba9081001e39011b8a250bb8b
igjnaj <155215699+igjnaj@users.noreply.github.com>
2024-04-06 22:22:11 +0700

    Update b.js

--- b.js
+++ b.js
@@ -1,1 +1,7 @@
-// Cool
+const searchKeywordElement = document.getElementById('searchkeyword');
+    
+// Check if the element exist
+if (searchKeywordElement) {
+// Change the text content of the element to "Tìm kiếm"
+    searchKeywordElement.textContent = "Tìm kiếm";
+}
