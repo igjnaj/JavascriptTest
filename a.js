13e37a7f295bea38604d1b2a5263f23904700433
igjnaj <155215699+igjnaj@users.noreply.github.com>
2024-04-07 11:05:35 +0700

    Update a.js

--- a.js
+++ a.js
@@ -3,7 +3,7 @@ var element = document.querySelector('footer p.Copy a');
 // Check if the element exists before making changes
 if (element) {
     // Change the HTML content of the element
-    element.innerHTML = "<strong>Modified by klemplus<strong/> 🔥<br>©️ Copyright 2024 Animevietsub.TV All rights reserved.";
+    element.innerHTML = "Modified by klemplus 🔥<br>©️ Copyright 2024 Animevietsub.TV All rights reserved.";
 }
 // Change the tab name
 document.title = "klemplus ❤️ this :)";
