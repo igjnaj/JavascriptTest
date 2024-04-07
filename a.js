0e30fcd7865837db2a54bac67e639fdbab7d9936
igjnaj <155215699+igjnaj@users.noreply.github.com>
2024-04-07 11:03:59 +0700

    Update a.js
    Remove trashb

--- a.js
+++ a.js
@@ -3,13 +3,7 @@ var element = document.querySelector('footer p.Copy a');
 // Check if the element exists before making changes
 if (element) {
     // Change the HTML content of the element
-    element.innerHTML = "<bold>Modified by klemplus<bold/> 🔥<br>©️ Copyright 2024 Animevietsub. All rights reserved.";
+    element.innerHTML = "<strong>Modified by klemplus<strong/> 🔥<br>©️ Copyright 2024 Animevietsub.TV All rights reserved.";
 }
 // Change the tab name
-document.title = "Animevietsub ❤️";
-// Change the favicon to a custom icon using a URL
-var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
-link.type = 'image/x-icon';
-link.rel = 'icon';
-link.href = 'https://raw.githubusercontent.com/igjnaj/JavascriptTest/main/B6F35539-8CC4-419D-B676-970B78B357D9.png';
-document.getElementsByTagName('head')[0].appendChild(link);
+document.title = "klemplus ❤️ this :)";
